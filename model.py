641f682fbe6f534aa61f3c236279f056f1cdb486
Shivanshu Purohit <shivanshup10@gmail.com>
2021-11-14 08:21:59 +0530

    Update model.py

--- model.py
+++ model.py
@@ -103,3 +103,45 @@ class BottleneckBlock(Module):
             x += layer(self.condition(self.norm(x), condition))
         x = rearrange(x, "b h w c -> b c h w")
         return x
+
+
+class Bicubic(Module):
+    def __init__(self, scale_factor):
+        super().__init__()
+        self.scale_factor = scale_factor
+    
+    def forward(self, x):
+        return F.interpolate(x, scale_factor=self.scale_factor, mode='bicubic')
+
+
+class UNet(Module):
+    def __init__(self, enc_dec: Sequence[Tuple[Module, Module]], bottleneck: Module):
+        super().__init__()
+        outer_pair, *inner_remaining = enc_dec
+        self.enc, self.dec = outer_pair
+        if inner_remaining:
+            self.bottleneck = UNet(inner_remaining, bottleneck)
+        else:
+            self.bottleneck = bottleneck
+
+    def forward(self, x, condition):
+        encoded = self.enc(x, condition)
+        bottlenecked = self.bottleneck(encoded, condition)
+        return self.dec(torch.cat([encoded, bottlenecked], dim=1), condition)
+
+
+class Model(Module):
+    def __init__(self):
+        super().__init__()
+        self.net = UNet([
+            (ResidualBlock(3, 64), ResidualBlock(64+64, 3)),
+            (ConditionedSequential(Bicubic(1/2), ResidualBlock(64, 128)), \
+                ConditionedSequential(Bicubic(1/2), ResidualBlock(128+128, 64), Bicubic(2))),
+            (ConditionedSequential(Bicubic(1/2), ResidualBlock(128, 256)), \
+                ConditionedSequential(Bicubic(1/2), ResidualBlock(256+256, 128), Bicubic(2))),
+            (ConditionedSequential(Bicubic(1/2), ResidualBlock(256, 512)), \
+                ConditionedSequential(Bicubic(1/2), ResidualBlock(512+512, 256), Bicubic(2))),
+        ], BottleneckBlock(512))
+    
+    def forward(self, x, condition):
+        return self.net(x, condition)
