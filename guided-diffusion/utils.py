04a2fedc4971f011bf7d255b76e7b6b0bbe35758
Shivanshu Purohit <shivanshup10@gmail.com>
2022-01-02 13:02:29 +0530

    Update utils.py

--- guided-diffusion/utils.py
+++ guided-diffusion/utils.py
@@ -26,4 +26,14 @@ def linear(*args, **kwargs):
     return nn.Linear(*args, **kwargs)
   
   
-  
+def conv_nd(dims, *args, **kwargs):
+    """
+    Create a 1D, 2D, or 3D convolution module.
+    """
+    if dims == 1:
+        return nn.Conv1d(*args, **kwargs)
+    elif dims == 2:
+        return nn.Conv2d(*args, **kwargs)
+    elif dims == 3:
+        return nn.Conv3d(*args, **kwargs)
+    raise ValueError(f"unsupported dimensions: {dims}")
